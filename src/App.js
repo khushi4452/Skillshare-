6756e2be5f5221d15b6c208c5bc071deb56a8d19
Musrat Khan <147485713+khushi4452@users.noreply.github.com>
2024-10-06 19:52:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1> Skillshare </h1>
+     <h1> Skillshare a beautiful platform where you can share your Knowledge </h1>
      
       </header>
     </div>
