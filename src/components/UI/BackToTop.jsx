f94dc21c513bd2b4bf4e850e07ad4056831d6370
Musrat Khan <147485713+khushi4452@users.noreply.github.com>
2024-10-08 20:05:46 +0530

    Update BackToTop.jsx

---
+++ src/components/UI/BackToTop.jsx
@@ -0,0 +1,16 @@
+const ScrollToTopButton = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', 
+    });
+  };
+
+  return (
+    <button onClick={scrollToTop} className="scroll-to-top-button">
+      <img src={Go} alt="Go to top" />
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
